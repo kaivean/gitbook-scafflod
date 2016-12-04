f377988c28ecaf0f66f04e51857c89bfc4b7844d
kaivean <kaivean@outlook.com>
2016-12-04 22:42:32 +0800

    udpate

--- _server/update.sh
+++ _server/update.sh
@@ -7,12 +7,14 @@ echo "udpate $DIR"
 # 尝试10次
 for x in 1
 do
-    res=`git pull origin master|grep -c 'Counting objects'`
+    res=`git pull origin master`
+    num=`echo $res|grep -c 'Counting objects'`
     # 不为1 即代表，代码有更新，那么就更新文档，重启服务器
     if [ "$res" -eq "1" ]
       then
           #   sh $DIR/start-online-server.sh
-            echo "success"
+          echo $res
+          echo "success"
       else
         echo "fail"
       fi

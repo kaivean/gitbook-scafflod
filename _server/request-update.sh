1602fd2b622bde20427eef3d3ca866b9a87d62fe
kaivean <kaivean@outlook.com>
2016-12-05 10:33:39 +0800

    udpate

--- _server/request-update.sh
+++ _server/request-update.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+cd `dirname $0`
+CURDIR=`pwd`
 
+# 到达git根目录
+cd ../
+GITPATH=`pwd`
 sleep 3
+
+#  得到服务器地址
+host=`cat package.json|grep '_serverHost'|tr -s [:space:]|cut -f 4 -d '"'`
+port=`cat package.json|grep '_serverPort'|tr -s [:space:]|cut -f 4 -d '"'`
+
 echo "********************************************"
 echo "requesting http://$host:$port/update to update the server"
 echo "********************************************"
@@ -9,13 +19,9 @@ notfound=`echo $res|grep -c 'Not Found'`
 updatesucess=`echo $res|grep -c 'update server success'`
 
 # 不为1 即代表，代码有更新，那么就更新文档，重启服务器
-if [ "$notfound" -eq "1" ] then
-    git pull origin master
-    gitbook build
+if [ "$notfound" -eq "1" ]; then
     echo "no server specified, please start firstly"
-    echo "gitbooksuccess"
-    break
-elif [ "$updatesucess" -eq "1" ] then
+elif [ "$updatesucess" -eq "1" ]; then
     echo $res
 else
     echo "if updating doesn't success, please open the url 'http://$host:$port/update' when pushing finishes"
